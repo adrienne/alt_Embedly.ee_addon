d75fbe32b78b7ef87f4d55827342eca20072dc51
Adrienne L. Travis <adrienne@utilitarienne.com>
2011-12-22 01:16:43 -0600

    added Embed.ly official PHP library

---
+++ system/expressionengine/third_party/alt_embedly/language/english/lang.alt_embedly.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(	
+	'alt_embedly_module_name' => 
+	'ALT Embedly',
+
+	'alt_embedly_module_description' => 
+	'Module for using Embed.ly',
+
+	'module_home' => 'ALT Embedly Home',
+	
+// Start inserting custom language keys/values here
+	
+);
+
+/* End of file lang.alt_embedly.php */
+/* Location: /system/expressionengine/third_party/alt_embedly/language/english/lang.alt_embedly.php */
